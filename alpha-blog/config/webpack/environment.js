1736da90ec64726ef66747c3e843654c64b0aabd
Alexander Beers <alexbeerz@yahoo.com>
2020-02-13 23:00:12 -0800

    Style Homepage and add navbar + footer

--- alpha-blog/config/webpack/environment.js
+++ alpha-blog/config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
-
+//recommended for rails 6?
+// const webpack = require('webpack')
+// enviroment.plugins.append('Provide', 
+// 	new webpack.ProvidePlugin({
+// 		$: 'jquery',
+// 		jquery: 'jquery'
+// 		Popper: ['popper.js', 'default']
+// 	})
 module.exports = environment

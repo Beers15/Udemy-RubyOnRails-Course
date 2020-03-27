2173a8b04c777b90da7f08eed1617ce8db017921
Alexander Beers <alexbeerz@yahoo.com>
2020-03-26 18:41:53 -0700

    Add admin functionality allowing admin users to delete users/posts.

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -61,7 +61,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def require_same_user
-		if current_user != @article.user 
+		if current_user != @article.user and !current_user.admin?
 			flash[:danger] = "You don't have permission to do that."
 			redirect_to root_path
 		end

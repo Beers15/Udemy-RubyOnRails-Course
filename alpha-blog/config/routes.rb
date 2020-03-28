01235ee87239edc10ffefbe4faa551672c2fb4bc
Alexander Beers <alexbeerz@yahoo.com>
2020-03-28 15:06:04 -0700

    Add categories model and tests.

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 	get 'about', to: 'pages#about'
 
 	get 'signup', to: 'users#new'
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
 	resources :users, except: [:new]
 
 	resources :articles
+
+	resources :categories, except: [:destroy]
 end
